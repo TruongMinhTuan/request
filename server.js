a41eaafca184665ccde6ed0cdea198cf7b95b2ec
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-11 15:06:56 +0700

    no message

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express       = require('express');
 const say           = require('say');
 const app           = express();
 app.get('/', function(req, res) {
-    res.writeHead(200, {
-        "Context-type" : "text/html"          
-        });
+    res.writeHead(200);
     message = (req.query && req.query.message) ? req.query.message : ' ';
     say.speak(message);
     res.end( message);
