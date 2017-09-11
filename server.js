d2b145e3da7d8711f86b99db3b1e87ebdea68ad7
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-11 17:42:10 +0700

    v2

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express       = require('express');
 const say           = require('say');
 const app           = express();
 app.get('/', function(req,res) {
-    res.writeHead(200);
-    message = (req.query && req.query.message) ? req.query.message : ' ';
-    say.speak(message);
-    res.end(message);
+        if (req.query.message == null || req.query.message == '') 
+        {
+            res.status(404)
+            res.end();
+        }
+      else
+        { 
+            res.status(200)
+            say.speak(req.query.message);
+            res.end();
+        }        
     });
 app.listen(8080);
