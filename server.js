acc088bad3f419ab232dbc86a5ea26cf7f77399a
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-13 08:55:36 +0700

    test

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express       = require('express');
 const say           = require('say');
 const app           = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/',function(req,res) {
         if (req.query.message == null){
             res.status(404)
@@ -14,8 +17,7 @@ app.get('/',function(req,res) {
             say.speak(req.query.message);
             res.end();
         }        
-    });
-    
+    });    
 app.post('/auto', function (req, res) {	
 	console.log(req.body.pusher.name + ' just pushed to ' + req.body.repository.name);
 	console.log('pulling code from GitHub...');
