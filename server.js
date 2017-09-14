6f09eaa97f902bdd88f5608ee7b7beffbbb248a5
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-14 09:51:00 +0700

    14/09/2007-8

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.post('/auto',function(req, res){
     console.log('repository: '+req.body['repository']['name'])
     console.log('pulling code from GitHub...');
     //-------------
-    console.log('---------------------reset-----------------');
-    exec('git -C \Users\Phuong\Desktop\request reset --hard');
-    console.log('---------------------clean-----------------');
-    exec('git -C \Users\Phuong\Desktop\request clean -df');
+ //   console.log('---------------------reset-----------------');
+  //  exec('git -C \Users\Phuong\Desktop\request reset --hard');
+  //  console.log('---------------------clean-----------------');
+ //   exec('git -C \Users\Phuong\Desktop\request clean -df');
     console.log('----------------------pull-----------------');
     exec('git -C \Users\Phuong\Desktop\request pull -f');
     execCallback();
