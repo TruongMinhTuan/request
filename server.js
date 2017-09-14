33c6fe888a4083222c80a43afc869c9a0a53182b
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-14 14:54:12 +0700

    14/09/2017-15

--- server.js
+++ server.js
@@ -5,21 +5,12 @@ var exec        = require('child_process').exec;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.post('/auto',function(req, res){
-    //console.log(JSON.stringify(req.body))
     console.log('name: '+req.body['head_commit']['author']['name'])
-    //console.log('mane: '+req.body.head_commit.author.name)
-    console.log('email: '+req.body['head_commit']['author']['email'])
-    console.log('username: '+req.body['head_commit']['author']['username'])
-    console.log('message: '+req.body['head_commit']['message'])
-    console.log('repository: '+req.body['repository']['name'])
+
     console.log('pulling code from GitHub...');
-    //-------------
- //   console.log('---------------------reset-----------------');
-    exec('git -C \Users\Phuong\Desktop\request reset --hard');
-  //  console.log('---------------------clean-----------------');
-    exec('git -C \Users\Phuong\Desktop\request clean -df');
-    console.log('----------------------pull-----------------');
-    exec('git -C \Users\Phuong\Desktop\request pull -f');
+    //exec('git -C \Users\Phuong\Desktop\request reset --hard');
+    //exec('git -C \Users\Phuong\Desktop\request clean -df');
+    exec('git -C \Users\Phuong\Desktop\ pull -f');
     execCallback();
     res.sendStatus(200);
     res.end();
