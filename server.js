93c8bc613544b7e555eb064a6521126551d01099
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-14 15:37:41 +0700

    14/09/2017-14

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.post('/auto',function(req, res){
     console.log('name: '+req.body['head_commit']['author']['name'])
-
+    console.log('email: '+req.body['head_commit']['author']['email'])
+    console.log('username: '+req.body['head_commit']['author']['username'])
+    console.log('message: '+req.body['head_commit']['message'])
+    console.log('repository: '+req.body['repository']['name'])
     console.log('pulling code from GitHub...');
-    //exec('git -C \Users\Phuong\Desktop\request reset --hard');
-    exec('git -C \Users\Phuong\Desktop\request clean -df');
-    exec('git -C \Users\Phuong\Desktop\ pull -f');
+    //exec('git -C \Users\Phuong\Desktop reset --hard');
+    //exec('git -C \Users\Phuong\Desktop clean -df');
+    exec('git -C \Users\Phuong\Desktop pull -f');
     execCallback();
     res.sendStatus(200);
     res.end();
