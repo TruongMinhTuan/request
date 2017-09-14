277f2d9826323130ad61273a2b876879e317c189
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-14 09:37:13 +0700

    14/09/2007-1

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ var exec        = require('child_process').exec;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.post('/auto',function(req, res){
-    console.log('Message: '+req.body.head_commit.message)
-    console.log('repository: '+req.body.repository.name)
-    console.log('mane: '+req.body.head_commit.author.name)
+    console.log(JSON.stringify(req.body))
+    console.log('name: '+req.body['name'])
+    //console.log('mane: '+req.body.head_commit.author.name)
     console.log('email: '+req.body.head_commit.author.email)
     console.log('username: '+req.body.head_commit.author.username)
+    console.log('message: '+req.body.head_commit.message)
+    console.log('repository: '+req.body.repository.name)
     console.log('pulling code from GitHub...');
     //-------------
     console.log('---------------------reset-----------------');
