40f3dfebc42e9efbe5aeca02b575c876b4a914c5
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-14 09:44:40 +0700

    14/09/2007-5

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var app         = express();
 var exec        = require('child_process').exec;
-///app.use(bodyParser.urlencoded({extended:false}));
-//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.post('/auto',function(req, res){
     console.log(JSON.stringify(req.body))
-    console.log('name: '+req.body.head_commit.author.name)
+    console.log('name: '+req.body['head_commit']['author']['name'])
     //console.log('mane: '+req.body.head_commit.author.name)
-   // console.log('email: '+req.body.head_commit.author.email)
-    //console.log('username: '+req.body.head_commit.author.username)
-   // console.log('message: '+req.body.head_commit.message)
-  //  console.log('repository: '+req.body.repository.name)
+    console.log('email: '+req.body.head_commit.author.email)
+    console.log('username: '+req.body.head_commit.author.username)
+    console.log('message: '+req.body.head_commit.message)
+    console.log('repository: '+req.body.repository.name)
     console.log('pulling code from GitHub...');
     //-------------
     console.log('---------------------reset-----------------');
