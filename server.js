6f1b22c565f570f05731c53dddf02c46e5c70389
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-18 15:01:17 +0700

    new

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/auto',function(req, res){
     console.log('name: '+req.body['head_commit']['author']['name'])
     console.log('message: '+req.body['head_commit']['message'])
  
-    exec('gti pull -f', (err, stdout, stderr) => {
+    exec('git pull -f', (err, stdout, stderr) => {
         if (err) {
           // node couldn't execute the command
           return;
