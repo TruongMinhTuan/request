d64c0e6735b056a7c615be5b73101f6e1ec2a8ca
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-18 11:38:14 +0700

    18/09/2017-8

--- server.js
+++ server.js
@@ -8,16 +8,10 @@ app.get('/',function(req,res){
     res.send('Xin Chao 2');
 })
 app.post('/auto',function(req, res){
-   // if(req.body=='' ||req.body== null){
-     //  Console.log(req.body)
-        console.log('name: '+req.body['head_commit']['author']['name'])
-        console.log('message: '+req.body['head_commit']['message'])
-        exec('git -C /home/tuantruong/git/request pull -f')
-        res.sendStatus(200);
-        res.end();//}
-   // else{
-      //  res.status(500)
-      //  res.end();}
-   
+    console.log('name: '+req.body['head_commit']['author']['name'])
+    console.log('message: '+req.body['head_commit']['message'])
+    exec('git -C /home/tuantruong/git/request pull -f')
+    res.sendStatus(200);
+    res.end();    
 });
 app.listen(8080);
