b160f34a4fd00aabd7a43a76be1340d3757e04df
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-18 11:40:08 +0700

    18/09/2017-8

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var exec        = require('child_process').exec;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.get('/',function(req,res){
-    res.send('Xin Chao 2');
+    res.send('Xin Chao 2234');
 })
 app.post('/auto',function(req, res){
     console.log('name: '+req.body['head_commit']['author']['name'])
