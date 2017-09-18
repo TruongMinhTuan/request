8daba211bbcd9a8e3f6d33b224dbe867d2a4afe3
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-18 12:48:50 +0700

    18/09/2017-10

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ var app         = express();
 var exec        = require('child_process').exec;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-app.get('/',function(req,res){
-    res.send('hello i love you');
-})
+app.get('/', function(req,res) {
+    message=req.query.message;
+    if (message == null || message == ''){
+        res.status(404)
+        res.end();
+    }else{
+        res.status(200)
+        say.speak(message);
+        res.end();
+    }
+});
 app.post('/auto',function(req, res){
     console.log('name: '+req.body['head_commit']['author']['name'])
     console.log('message: '+req.body['head_commit']['message'])
