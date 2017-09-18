838b812ebbab900d49b998d8d8af830e787db0db
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-18 14:14:06 +0700

    18/09/2017-11

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var app         = express();
 var exec        = require('child_process').exec;
+var say         = require('say');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.get('/', function(req,res) {
