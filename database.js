4c122a0ef89adfa7cc13ee316d9286d632e47c89
Tuan Truong <truongvanminhtuan@gmail.com>
2017-09-20 16:31:36 +0700

    20/09-1

--- database.js
+++ database.js
@@ -6,22 +6,23 @@ const messageChema =new mongoose.Schema({
 const messageModel = mongoose.model('message',messageChema)
 module.exports = {
     connect: function (url){
-        mongoose.connect(url,{
+        var db = mongoose.connect(url, {
             useMongoClient: true,
         });
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     },
     add: function(message,createdAt) {
-        messageModel.create({
-            message :message,
-            createdAt :createdAt
-        })
+        new messageModel({
+            message: message,
+            createdAt :createdAt 
+        }).save()
     },
-    show:function() {
-        messageModel.find().exec((err,result)=> {
-            for(i=0;i<result.length;i++)
-                console.log('no:'+i+' createAt: '+result[i].createdAt+'message: '+result[i].message)
-        })
-    }
+    // show:function() {
+    //     messageModel.find().exec((err,result)=> {
+    //         for(i=0;i<result.length;i++)
+    //             console.log('no:'+ i +' createAt: ' + result[i].createdAt + 'message: ' + result[i].message)
+    //     })
+    // }
 };
 
 
